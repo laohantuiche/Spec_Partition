1839656d863c473c21cf931c49dc0b4c506b9d86
Hua Zonghan <2365326076@qq.com>
2020-06-20 22:50:59 +0800

    SVM

---
+++ support_vector_machine.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pandas_profiling
+from sklearn import svm
+import joblib
+
+data = pd.read_csv("ST_or_CS_44.csv", sep=',')
+"""
+profile = data.profile_report(title="relation")
+profile.to_file(output_file="classifier analysis.html")
+"""
+
+features_arr = ["CPU_Utilization", "Frequency", "IPC", "Misses", "LLC", "MBL",
+                "Memory_Footprint", "Virt_Memory", "Res_Memory", "Allocated_Cache", "ST"]
+
+data = (data-data.min())/(data.max()-data.min())
+input_features = data.loc[:, features_arr[:10]].values
+output_target = data.loc[:, features_arr[10:]].values
+
+classifier = svm.SVC()
+classifier.fit(input_features, output_target)
+print(classifier.score(input_features, output_target))
+
+joblib.dump(classifier, "svm.pkl")
